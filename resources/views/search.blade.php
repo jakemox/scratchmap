c0f913d9d3308dedc6bf419a1d73c2784adb8ee2
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-08 21:28:36 +0100

    finished search background

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,6 +7,9 @@
 <div class="sky">
     <div class="clouds"></div>
     <div class="mountains"></div>
+    <div class="trees"></div>
+    <div class="slope"></div>
+    <div class="fade"></div>
 </div>
 
 
