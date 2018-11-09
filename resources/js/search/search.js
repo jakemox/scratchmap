b865633a9bed243d260b1c07b44563dd48a4ea8b
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-10 00:41:13 +0100

    improved JS search

--- resources/js/search/search.js
+++ resources/js/search/search.js
@@ -9,14 +9,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     form.addEventListener('mouseover', function () {
         label.innerHTML = `<img src="\\img\\search-black.svg" alt="">`;
-        slope.style.left = '-10%';
+        slope.style.left = '-5%';
+        mountains.style.width = '110%';
+        mountains.style.left = '-5%';
+        trees.style.left = '5%';
         
         if (x.matches) {
-            trees.style.left = '10%';
             mountains.style.height = '45vh';
             mountains.style.bottom = '5vh';
         } else {
-            trees.style.left = 'calc(((100vw - 768px) / 2) + 10%)';
             mountains.style.height = '60vw';
             mountains.style.maxHeight = '80vh';
             mountains.style.bottom = '0';
@@ -26,13 +27,14 @@ document.addEventListener('DOMContentLoaded', function () {
     form.addEventListener('mouseleave', function () {
         label.innerHTML = `<img src="\\img\\search.svg" alt="">`;
         slope.style.left = '0';
+        mountains.style.width = '100%';
+        mountains.style.left = '0';
+        trees.style.left = '0';
 
         if (x.matches) {
-            trees.style.left = '0';
             mountains.style.height = '40vh';
             mountains.style.bottom = '10vh';
         } else {
-            trees.style.left = 'calc((100vw - 768px) / 2)';
             mountains.style.height = '50vw';
             mountains.style.maxHeight = '70vh';
             mountains.style.bottom = '0';
