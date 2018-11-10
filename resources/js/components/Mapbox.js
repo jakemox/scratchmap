b6e14b41844e0e3464d869cebde54bf1ce7049d1
Tomi Holstila <tomi.holstila@gmail.com>
2018-11-10 11:51:19 +0100

    map loads scratchmap style

--- resources/js/components/Mapbox.js
+++ resources/js/components/Mapbox.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import mapboxgl from 'mapbox-gl'
 import ReactMapGL from 'react-map-gl';
+import { fromJS } from 'immutable';
+
+const defaultMapStyle = "mapbox://styles/jakemox99/cjo4t22t8012s2sp2xwb0e0j5";
 
 const token = 'pk.eyJ1IjoiamFrZW1veDk5IiwiYSI6ImNqbmxtYjlvcjFtZmozcHE5aW9zN3pjeXcifQ.UCUt8f58HwBvpHcTz8JqkA';
 mapboxgl.accessToken = token
@@ -11,9 +14,10 @@ console.log("example.js loaded")
 export default class Mapbox extends React.Component {
 
   state = {
+    mapStyle: defaultMapStyle,
     viewport: {
-      width: 400,
-      height: 400,
+      width: "100%",
+      height: "100%",
       latitude: 37.7577,
       longitude: -122.4376,
       zoom: 8
@@ -21,8 +25,11 @@ export default class Mapbox extends React.Component {
   };
 
   render() {
+    const {mapStyle} = this.state; 
     return (
       <ReactMapGL
+
+        mapStyle={mapStyle}
         mapboxApiAccessToken='pk.eyJ1IjoiamFrZW1veDk5IiwiYSI6ImNqbmxtYjlvcjFtZmozcHE5aW9zN3pjeXcifQ.UCUt8f58HwBvpHcTz8JqkA'
         {...this.state.viewport}
         onViewportChange={(viewport) => this.setState({ viewport })}
@@ -30,7 +37,5 @@ export default class Mapbox extends React.Component {
     );
   }
 }
-
-
 
 ReactDOM.render(<Mapbox />, document.getElementById('map'));
