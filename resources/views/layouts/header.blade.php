5282b95f05938bfd6d554186bc11d650a0a847c0
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-14 21:39:55 +0100

    A few styling tweaks

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,26 +7,24 @@
   <div class="desktop">
     <div class="column1">
         <a href="/"><h2>Map</h2></a>
-        <a href="/search"><h2>Plan</h2></a>
-        @guest
-        <div class="log_reg">
-                <a href="{{ route('register') }}">{{ __('Register') }}</a>
-        </div>
-        @endguest
+        <a href="/search"><h2>Search</h2></a>
     </div>
     <div class="column2">
         <div class="balloon"><img src="\img\{{$image}}.svg" alt="balloon"></div>
         <h1>ScratchMapp</h1>
     </div>
     <div class="column3">
-        @auth
-        <a href="/profile"><h2>Profile</h2></a>
-        @endauth
-        @guest
-    <a href="{{ route('login') }}"><h2>Login</h2></a>
-        @endguest
     @auth
-    <a href="{{ url('/logout') }}"><h2>Logout</h2></a>
+        <a href="/profile"><h2>Profile</h2></a>
+    @endauth
+    @guest
+        <a href="{{ route('register') }}"><h2>{{ __('Register') }}</h2></a>
+    @endguest
+    @guest
+        <a href="{{ route('login') }}"><h2>Login</h2></a>
+    @endguest
+    @auth
+        <a href="{{ url('/logout') }}"><h2>Logout</h2></a>
     @endauth
         
     </div> 
