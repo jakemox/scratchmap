57e2cf109beb2439498b750276efdd4e90442afd
mmilic1994 <33220095+mmilic1994@users.noreply.github.com>
2018-11-14 23:49:56 +0100

    profile page almost working

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,101 +1,131 @@
 @extends('layouts.layout', [
-    'image' => 'balloon',
-    'css' => 'app'
+    'image' => 'balloon-cutout',
+    'css' => 'plan'
 ])
 
 @section('content')
 
-<link href="css-circular-prog-bar.css" rel="stylesheet">
+
 
 <div class="profile">
-      <h2>{{$user_name}}</h2>
-      <h3>Score: {{$user_score}}</h3>
-      <h3>Statistics</h3>
-  <h3>Visited countries: {{count($visited_countries)}}</h3>
-<div class="africa">
-  <h4>Africa: {{$africa}}/53 (<?php echo round(($africa/53)*100, 2) ;?>%)</h4>
-  <div class="progress-pie-chart" data-percent="">
-    <div class="ppc-progress">
-      <div class="ppc-progress-fill"></div>
-    </div>
-    <div class="ppc-percents">
-      <div class="pcc-percents-wrapper">
-        <img src="\img\africa-01.svg" height="150px">
+  <div class="fade">
+    <div class="outer">
+    <div class="form">
+      <div class="profile_details">
+        <div class="details">
+          <h2>{{$user_name}}</h2>
+          <h3>Score: {{$user_score}}</h3>
+          <h3>Statistics</h3>
+          <h3>Visited countries: {{count($visited_countries)}}</h3>
+          <h4>Africa: {{$africa}}/53 (<?php echo round(($africa/53)*100, 2) ;?>%)</h4>
+          <h4>Asia: {{$asia}}/47 (<?php echo round(($asia/47)*100, 2) ;?>%)</h4>
+          <h4>Europe: {{$europe}}/45 (<?php echo round(($europe/45)*100, 2) ;?>%)</h4>
+          <h4>North America: {{$north_america}}/23 (<?php echo round(($north_america/23)*100, 2) ;?>%)</h4>
+          <h4>South America: {{$south_america}}/12 (<?php echo round(($south_america/12)*100, 2) ;?>%)</h4>
+          <h4>Oceania: {{$australia}}/14 (<?php echo round(($australia/14)*100, 2) ;?>%)</h4>
+        </div>
+        <div class="profile_image">
+            <img src="" height="200" alt="Image preview...">
+            <input type="file" onchange="previewFile()"><br>
+            
+        </div>
       </div>
-    </div>
+      <div class="continents-profile">
+          <div class="africa">
+              <div class="progress-pie-chart" data-percent="">
+                <div class="ppc-progress">
+                  <div class="ppc-progress-fill"></div>
+                </div>
+                <div class="ppc-percents">
+                  <div class="pcc-percents-wrapper">
+                    <img src="\img\africa-01.svg" height="120px">
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="asia">
+                <div class="progress-pie-chart" data-percent="">
+                    <div class="ppc-progress">
+                      <div class="ppc-progress-fill"></div>
+                    </div>
+                    <div class="ppc-percents">
+                      <div class="pcc-percents-wrapper">
+                        <img src="\img\asia-01.svg" height="120px">
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            
+            
+            <div class="europe">
+                <div class="progress-pie-chart" data-percent="">
+                    <div class="ppc-progress">
+                      <div class="ppc-progress-fill"></div>
+                    </div>
+                    <div class="ppc-percents">
+                      <div class="pcc-percents-wrapper">
+                        <img src="\img\europe-01.svg" height="120px">
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            
+            <div class="north_america">
+                <div class="progress-pie-chart" data-percent="">
+                    <div class="ppc-progress">
+                      <div class="ppc-progress-fill"></div>
+                    </div>
+                    <div class="ppc-percents">
+                      <div class="pcc-percents-wrapper">
+                        <img src="\img\north-america-01.svg" height="120px">
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            
+            <div class="south_america">
+                <div class="progress-pie-chart" data-percent="">
+                    <div class="ppc-progress">
+                      <div class="ppc-progress-fill"></div>
+                    </div>
+                    <div class="ppc-percents">
+                      <div class="pcc-percents-wrapper">
+                        <img src="\img\south-america-01.svg" height="120px">
+                      </div>
+                    </div>
+                  </div>
+            </div>
+            
+            <div class="australia">
+                <div class="progress-pie-chart" data-percent="">
+                    <div class="ppc-progress">
+                      <div class="ppc-progress-fill"></div>
+                    </div>
+                    <div class="ppc-percents">
+                      <div class="pcc-percents-wrapper">
+                        <img src="\img\oceania-01.svg" height="120px">
+                      </div>
+                    </div>
+                  </div>
+            </div> 
+  
+      </div>
+      
+  
+    @foreach ($visited_countries as $item)
+    <div class="country-list">
+      <div class="image-crop">
+          <img class="flag-icon" src="img/flags-normal/{{\App\Country::find($item->country_id)->code}}.png" alt="{{\App\Country::find($item->country_id)->code}}">
+      </div>
+      <div class="list-country-name">
+          {{\App\Country::find($item->country_id)->name}}
+      </div>
+      </div> 
+    @endforeach
   </div>
-</div>
-
-<div class="asia">
-  <h4>Asia: {{$asia}}/47 (<?php echo round(($asia/47)*100, 2) ;?>%)</h4>
-  <div class="ppc-progress">
-      <div class="ppc-progress-fill"></div>
-    </div>
-    <div class="ppc-percents">
-      <div class="pcc-percents-wrapper">
-        <img src="\img\asia-01.svg" height="150px">
-      </div>
-    </div>
   </div>
-
-
-<div class="europe">
-  <h4>Europe: {{$europe}}/45 (<?php echo round(($europe/45)*100, 2) ;?>%)</h4>
-  <div class="ppc-progress">
-      <div class="ppc-progress-fill"></div>
-    </div>
-    <div class="ppc-percents">
-      <div class="pcc-percents-wrapper">
-        <img src="\img\europe-01.svg" height="150px">
-      </div>
-    </div>
-</div>
-
-<div class="north_america">
-  <h4>North America: {{$north_america}}/23 (<?php echo round(($north_america/23)*100, 2) ;?>%)</h4>
-  <div class="ppc-progress">
-      <div class="ppc-progress-fill"></div>
-    </div>
-    <div class="ppc-percents">
-      <div class="pcc-percents-wrapper">
-        <img src="\img\north-america-01.svg" height="150px">
-      </div>
-    </div>
-</div>
-
-<div class="south_america">
-  <h4>South America: {{$south_america}}/12 (<?php echo round(($south_america/12)*100, 2) ;?>%)</h4>
-  <div class="ppc-progress">
-      <div class="ppc-progress-fill"></div>
-    </div>
-    <div class="ppc-percents">
-      <div class="pcc-percents-wrapper">
-        <img src="\img\south-america-01.svg" height="150px">
-      </div>
-    </div>
-</div>
-
-<div class="australia">
-  <h4>Oceania: {{$australia}}/14 (<?php echo round(($australia/14)*100, 2) ;?>%)</h4>
-  <div class="ppc-progress">
-      <div class="ppc-progress-fill"></div>
-    </div>
-    <div class="ppc-percents">
-      <div class="pcc-percents-wrapper">
-        <img src="\img\oceania-01.svg" height="150px">
-      </div>
-    </div>
-</div> 
-  @foreach ($visited_countries as $item)
-  <div class="country-list">
-    <div class="image-crop">
-        <img class="flag-icon" src="img/flags-normal/{{\App\Country::find($item->country_id)->code}}.png" alt="{{\App\Country::find($item->country_id)->code}}">
-    </div>
-    <div class="list-country-name">
-        {{\App\Country::find($item->country_id)->name}}
-    </div>
-    </div> 
-  @endforeach
+  </div>
 </div>
 
 @endsection
@@ -108,6 +138,26 @@
   let north_america =  ({{$north_america}}/23)*100;
   let south_america =  ({{$south_america}}/12)*100;
   let australia =  ({{$australia}}/14)*100;
+
+ 
+   function previewFile(){
+       var preview = document.querySelector('img'); //selects the query named img
+       var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+       var reader  = new FileReader();
+
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
+
+       if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       } else {
+           preview.src = "";
+       }
+  }
+
+  previewFile();  //calls the function named previewFile()
+  
 
 
   $(function(){
