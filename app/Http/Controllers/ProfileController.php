8c386891a4ca3796a3731cabf8923fc82e535831
mmilic1994 <33220095+mmilic1994@users.noreply.github.com>
2018-11-14 13:34:21 +0100

    profile page up and running

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,9 +14,15 @@ class ProfileController extends Controller
         $user_score = $user->score;
         $user_name = $user->name;
         
+        $europe = $user->countries()->where('CONTINENT', 'Europe')->count();
+        $africa = $user->countries()->where('CONTINENT', 'Africa')->count();
+        $asia = $user->countries()->where('CONTINENT', 'Asia')->count();
+        $north_america = $user->countries()->where('CONTINENT', 'North America')->count();
+        $south_america = $user->countries()->where('CONTINENT', 'South America')->count();
+        $australia = $user->countries()->where('CONTINENT', 'Oceania')->count();
+
 
         $visited_countries = DB::table('user_visited_countries')->where('user_id', $user_id)->get();
-    // dd(DB::table('user_visited_countries')->where('user_id', $user_id)->get());
-        return view('profile',  compact('user_score', 'visited_countries', 'user_name'));
+        return view('profile',  compact('user_score', 'visited_countries', 'user_name', 'europe', 'africa', 'asia', 'north_america', 'south_america', 'australia'));
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,8 +10,16 @@
 <div class="profile">
   <aside class="aside">
       <h2>{{$user_name}}</h2>
-  <h3>Score: {{$user_score}}</h3>
+      <h3>Score: {{$user_score}}</h3>
+      <h3>Statistics</h3>
   <h3>Visited countries: {{count($visited_countries)}}</h3>
+  <h4>Africa: {{$africa}} (<?php echo round(($africa/53)*100, 2) ;?>%)</h4>
+  <h4>Asia: {{$asia}} (<?php echo round(($asia/47)*100, 2) ;?>%)</h4>
+  <h4>Europe: {{$europe}} (<?php echo round(($europe/45)*100, 2) ;?>%)</h4>
+  <h4>North America: {{$north_america}} (<?php echo round(($north_america/23)*100, 2) ;?>%)</h4>
+  <h4>South America: {{$south_america}} (<?php echo round(($south_america/12)*100, 2) ;?>%)</h4>
+  <h4>Oceania: {{$australia}} (<?php echo round(($australia/14)*100, 2) ;?>%)</h4>
+  
   @foreach ($visited_countries as $item)
   <div class="country-list">
     <div class="image-crop">
@@ -22,6 +30,7 @@
     </div>
     </div> 
   @endforeach
+  
   </aside>
 </div>
 
