0b76f4c830bebe7740c57325b66489bfa8991f73
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-14 18:35:14 +0100

    continent logos

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,11 +1,23 @@
 <div class="container">
   <div class="continents-list" id="continents">
-    <a href="javascript:void(0)" class="continent" id="Africa">Africa</a>
-    <a href="javascript:void(0)" class="continent" id="Asia">Asia</a>
-    <a href="javascript:void(0)" class="continent" id="Europe">Europe</a>
-    <a href="javascript:void(0)" class="continent" id="North America">N. America</a>
-    <a href="javascript:void(0)" class="continent" id="Oceania">Oceania</a>
-    <a href="javascript:void(0)" class="continent" id="South America">S. America</a>
+    <a href="javascript:void(0)" class="continent" id="Africa">
+      <img src="\img\africa-01.svg" alt="">
+    </a>
+    <a href="javascript:void(0)" class="continent" id="Asia">
+      <img src="\img\asia-01.svg" alt="">
+    </a>
+    <a href="javascript:void(0)" class="continent" id="Europe">
+        <img src="\img\europe-01.svg" alt="">
+    </a>
+    <a href="javascript:void(0)" class="continent" id="North America">
+      <img src="\img\north-america-01.svg" alt="">
+    </a>
+    <a href="javascript:void(0)" class="continent" id="Oceania">
+        <img src="\img\oceania-01.svg" alt="">
+    </a>
+    <a href="javascript:void(0)" class="continent" id="South America">
+        <img src="\img\south-america-01.svg" alt="">
+    </a>
   </div>
   <ul id="country-list"></ul>
 </div>
