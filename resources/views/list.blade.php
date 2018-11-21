b0ed59b0f599580baad98d37fca6389f8e869650
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-21 10:30:27 +0100

    added some styling to features

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,22 +1,22 @@
 <div class="container">
   <div class="continents-list" id="continents">
     <a href="javascript:void(0)" class="continent" id="Africa">
-      <img src="\img\africa-01.svg" alt="">
+      <img src="\img\africa-01.png" alt="">
     </a>
     <a href="javascript:void(0)" class="continent" id="Asia">
-      <img src="\img\asia-01.svg" alt="">
+      <img src="\img\asia-01.png" alt="">
     </a>
     <a href="javascript:void(0)" class="continent" id="Europe">
-        <img src="\img\europe-01.svg" alt="">
+        <img src="\img\europe-01.png" alt="">
     </a>
     <a href="javascript:void(0)" class="continent" id="North America">
-      <img src="\img\north-america-01.svg" alt="">
+      <img src="\img\north-america-01.png" alt="">
     </a>
     <a href="javascript:void(0)" class="continent" id="Oceania">
-        <img src="\img\oceania-01.svg" alt="">
+        <img src="\img\oceania-01.png" alt="">
     </a>
     <a href="javascript:void(0)" class="continent" id="South America">
-        <img src="\img\south-america-01.svg" alt="">
+        <img src="\img\south-america-01.png" alt="">
     </a>
   </div>
   <ul id="country-list"></ul>
