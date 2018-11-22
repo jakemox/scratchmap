09afbf7290c46f77c7a095cf4f88a9dad74a292a
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-22 01:34:07 +0100

    styling on list view

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,23 +1,27 @@
 <div class="container">
   <div class="continents-list" id="continents">
-    <a href="javascript:void(0)" class="continent" id="Africa">
-      <img src="\img\africa-01.png" alt="">
-    </a>
-    <a href="javascript:void(0)" class="continent" id="Asia">
-      <img src="\img\asia-01.png" alt="">
-    </a>
-    <a href="javascript:void(0)" class="continent" id="Europe">
-        <img src="\img\europe-01.png" alt="">
-    </a>
-    <a href="javascript:void(0)" class="continent" id="North America">
-      <img src="\img\north-america-01.png" alt="">
-    </a>
-    <a href="javascript:void(0)" class="continent" id="Oceania">
-        <img src="\img\oceania-01.png" alt="">
-    </a>
-    <a href="javascript:void(0)" class="continent" id="South America">
-        <img src="\img\south-america-01.png" alt="">
-    </a>
+    <div class="continents-1">
+      <a href="javascript:void(0)" class="continent" id="Africa">
+        <img src="\img\africa-01.png" alt="">
+      </a>
+      <a href="javascript:void(0)" class="continent" id="Asia">
+        <img src="\img\asia-01.png" alt="">
+      </a>
+      <a href="javascript:void(0)" class="continent" id="Europe">
+          <img src="\img\europe-01.png" alt="">
+      </a>
+    </div>
+    <div class="continents-2">
+      <a href="javascript:void(0)" class="continent" id="North America">
+        <img src="\img\north-america-01.png" alt="">
+      </a>
+      <a href="javascript:void(0)" class="continent" id="Oceania">
+          <img src="\img\oceania-01.png" alt="">
+      </a>
+      <a href="javascript:void(0)" class="continent" id="South America">
+          <img src="\img\south-america-01.png" alt="">
+      </a>
+    </div>
   </div>
   <ul id="country-list"></ul>
 </div>
