ce78026fff9d37f4b70c04000058b405b6ad69da
Jake Moxon <jakemoxon@Jakes-MacBook-Air.local>
2018-11-22 16:13:13 +0100

    styled search page and list view

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,14 +10,14 @@
     <div id="mountains" class="mountains"></div>
     <div id="trees" class="trees"></div>
     <div id="slope" class="slope"></div>
-    <div class="fade">
+    <div class="search-fade">
         <form class="search-form" method="post" id="search-form" action="/city/search">
             {{csrf_field()}}
-            <div class="form-group" id="form-group">
+            <div class="form-group-search" id="form-group">
                 <label id="search-label" for="search">
                     <img src="\img\search.svg" alt="search-icon">
                 </label>
-                <input autocomplete="off" id="search-input" name="search" type="text" placeholder="Search destinations">
+                <input class="search-input" autocomplete="off" id="search-input" name="search" type="text" placeholder="Search destinations">
             </div>
         <div id="suggestions"></div>
         </form>
