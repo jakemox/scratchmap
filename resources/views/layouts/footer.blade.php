ef21e86913cddadefa70a9c2a8f1340439737442
mmilic1994 <mmilic1994@gmail.com>
2018-11-22 18:39:34 +0100

    some visual tweaks

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,6 +5,8 @@
     @auth
     <a href="/profile"><img src="/img/profile.svg" alt="profile" height="40px" width="40px"></a>
     @endauth
+    <a href="/register"><img src="/img/padlock.svg" alt="register" height="40px" width="40px"></a>
+    <a href="/login"><img src="/img/login.svg" alt="login" height="40px" width="40px"></a>
   </nav>
 </footer>
 
